9a5430152908e4b2bd0e396c3441718576bd59b8
grmkris <kristjan.grm1@gmail.com>
2024-05-26 08:13:05 +0200

    lfg

--- my-app/src/app/fhevm.ts
+++ my-app/src/app/fhevm.ts
@@ -1,12 +1,8 @@
 // Sauce: https://github.com/zama-ai/fhevmjs-next-template/blob/main/app/fhevmjs.ts
-import { BrowserProvider, AbiCoder } from "ethers";
+import {BrowserProvider, AbiCoder, Signer} from "ethers";
 import { initFhevm, createInstance, FhevmInstance } from "fhevmjs";
 
 const FHE_LIB_ADDRESS = "0x000000000000000000000000000000000000005d";
-
-export const init = async () => {
-  await initFhevm();
-};
 
 let instance: FhevmInstance;
 
@@ -47,3 +43,26 @@ export const getSignature = async (
 export const getInstance = () => {
   return instance;
 };
+
+export const getInstanceDynamically = async (props?: {
+}) => {
+  await initFhevm();
+  const provider = new BrowserProvider((window as any).ethereum);
+  const network = await provider.getNetwork();
+  const chainId = +network.chainId.toString();
+  console.log("chainId", {
+    chainId,
+  network,
+  });
+  const ret = await provider.call({
+    to: FHE_LIB_ADDRESS,
+    // first four bytes of keccak256('fhePubKey(bytes1)') + 1 byte for library
+    data: "0xd9d47bb001",
+  });
+  console.log("ret", ret);
+  const decoded = AbiCoder.defaultAbiCoder().decode(["bytes"], ret);
+  const publicKey = decoded[0];
+  console.log("publicKey", publicKey);
+  const dynamicinstance = await createInstance({ chainId, publicKey });
+  return dynamicinstance;
+}
